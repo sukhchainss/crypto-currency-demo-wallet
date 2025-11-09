2e5f13114f08b84d0852fbae222dd863664bba79
sukhchainsingh <sukhchainsingh@sukhchainsinghs-MacBook-Air>
2025-11-09 17:35:02 +0800

    Fix deps

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-    '@babel/preset-typescript',
-  ],
+module.exports = function(api) {
+  api.cache(true);
+  
+  // Use different presets for test vs production
+  const isTest = process.env.NODE_ENV === 'test';
+  
+  if (isTest) {
+    return {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript',
+      ],
+    };
+  }
+  
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
